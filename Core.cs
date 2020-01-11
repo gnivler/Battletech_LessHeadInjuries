c9d5d7316c812ce9be9548e66af233b50646cf81
gnivler <gnivil@gmail.com>
2020-01-10 21:08:31 -0600

    1.2.4

--- Core.cs
+++ Core.cs
@@ -1,114 +1,148 @@
-﻿using Newtonsoft.Json;
-using BattleTech;
-using Harmony;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using Random = System.Random;
+using BattleTech;
+using Harmony;
+using Newtonsoft.Json;
+using static FewerHeadInjuries.FewerHeadInjuries;
 
-//code taken and modified from: https://github.com/Mpstark/LessPilotInjuries
-//code taken and modified from: https://github.com/RealityMachina/Battletech_LessHeadInjuries
+// ReSharper disable InconsistentNaming
 
-public static class FewerHeadInjuries
+namespace FewerHeadInjuries
 {
-    //public static float ArmorHeadHitIgnoreDamageBelow = 10;
-    //public static float StructHeadHitIgnoreDamageBelow = 5;
-    internal static Settings Settings;
-    public static HashSet<Pilot> IgnoreNextHeadHit = new HashSet<Pilot>();
 
-    public static void Init(string settings)
+    //code taken and modified from: https://github.com/Mpstark/LessPilotInjuries
+    //code taken and modified from: https://github.com/RealityMachina/Battletech_LessHeadInjuries
+    public static class FewerHeadInjuries
     {
-        var harmony = HarmonyInstance.Create("com.btmodders.FewerHeadInjuries");
-        harmony.PatchAll(Assembly.GetExecutingAssembly());
+        internal static Settings Settings;
+        public static HashSet<Pilot> IgnoreNextHeadHit = new HashSet<Pilot>();
 
-        try
+        public static void Init(string settings)
         {
-            Settings = JsonConvert.DeserializeObject<Settings>(settings);
+            try
+            {
+                Settings = JsonConvert.DeserializeObject<Settings>(settings);
+            }
+            catch (Exception)
+            {
+                Settings = new Settings();
+            }
+
+            var harmony = HarmonyInstance.Create("com.btmodders.FewerHeadInjuries");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
-        catch (Exception)
+
+        public static void Log(object input)
         {
-            Settings = new Settings();
+            //FileLog.Log($"[FewerHeadInjuries] {input}");
         }
+
+        public static void Reset() => IgnoreNextHeadHit = new HashSet<Pilot>();
     }
 
-    public static void Reset() => IgnoreNextHeadHit = new HashSet<Pilot>();
-}
 
-public class Settings
-{
-    public bool EnableDebug = false;
-}
-
-[HarmonyPatch(typeof(SimGameState), "_OnFirstPlayInit")]
-public static class SimGameState__OnFirstPlayInit_Patch
-{
-    public static void Postfix(SimGameState __instance)
+    public class Settings
     {
-        // change a custom difficulty setting into a tag 
-        var sim = UnityGameInstance.BattleTechGame.Simulation;
-
-        if (sim.Constants.Story.MaximumDebt == 42)
-            sim.Constants.Story.MaximumDebt = 0;
-        else
-            sim.CompanyTags.Add("FewerHeadInjuriesDisabled");
+        public bool EnableDebug = false;
     }
-}
 
-[HarmonyPatch(typeof(BattleTech.GameInstance), "LaunchContract", new Type[] {typeof(Contract), typeof(string)})]
-public static class PatchLaunchContract
-{
-    static void Postfix()
+//[HarmonyPatch(typeof(SimGameState), "_OnFirstPlayInit")]
+//public static class SimGameState__OnFirstPlayInit_Patch
+//{
+//    public static void Prefix(SimGameState __instance, ref float __state)
+//    {
+//        //var fieldInfo =  __instance.Constants.Story.CompanyEventStartingChance;
+//        __state = __instance.Constants.Story.CompanyEventStartingChance;
+//    }
+//
+//    public static void Postfix(SimGameState __instance, float __state)
+//    {
+//        Log("Init: " + __instance.Constants.Story.CompanyEventStartingChance);
+//        // change a custom difficulty setting into a tag
+//        if (__instance.Constants.Story.CompanyEventStartingChance == 42)
+//        {
+//            __instance.CompanyTags.Add("FewerHeadInjuriesDisabled");
+//            __instance.Constants.Story.CompanyEventStartingChance = __state;
+//        }
+//    }
+//}
+
+    [HarmonyPatch(typeof(GameInstance), "LaunchContract", typeof(Contract), typeof(string))]
+    public static class GameInstance_LaunchContract_Patch
     {
         // reset on new contracts
-        FewerHeadInjuries.Reset();
+        public static void Postfix() => Reset();
     }
-}
 
-[HarmonyPatch(typeof(BattleTech.Mech), "DamageLocation")]
-public static class PatchDamageLocation
-{
-    static void Prefix(Mech __instance, ArmorLocation aLoc, float totalDamage)
+    [HarmonyPatch(typeof(Mech), "DamageLocation")]
+    public static class Mech_DamageLocation_Patch
     {
-        if (aLoc == ArmorLocation.Head)
+        static void Prefix(Mech __instance, ArmorLocation aLoc, float totalArmorDamage, float directStructureDamage)
         {
-            var sim = UnityGameInstance.BattleTechGame.Simulation;
-            if (sim.CompanyTags.Contains("FewerHeadInjuriesDisabled"))
-                return;
+            if (aLoc == ArmorLocation.Head)
+            {
+                //var sim = UnityGameInstance.BattleTechGame.Simulation;
+                //if (sim.CompanyTags.Contains("FewerHeadInjuriesDisabled"))
+                //    return;
 
-            var currentArmor = __instance.GetCurrentArmor(aLoc);
-            var maxArmor = __instance.GetMaxArmor(aLoc);
-            if (__instance.GetCurrentArmor(aLoc) == 0)
-                return;
+                var currentArmor = __instance.GetCurrentArmor(aLoc);
+                var maxArmor = __instance.GetMaxArmor(aLoc);
+                if (Math.Abs(__instance.GetCurrentArmor(aLoc)) < float.Epsilon)
+                {
+                    return;
+                }
 
-            if (currentArmor - totalDamage <= 0)
-                return;
+                if (currentArmor - totalArmorDamage + directStructureDamage <= 0)
+                {
+                    return;
+                }
 
-            float Modifier = UnityGameInstance.BattleTechGame.Simulation.GetCareerModeOverallDifficultyMod();
-            var rng = new Random().Next(1, 101);
-            if (rng >= currentArmor / maxArmor * 100 * Modifier)
-                FewerHeadInjuries.IgnoreNextHeadHit.Add(__instance.pilot);
+                var Modifier =
+                    UnityGameInstance.BattleTechGame.Simulation.GetCareerModeOverallDifficultyMod();
+                var rng = new Random().Next(1, 101);
+                if (rng <= currentArmor / maxArmor * 100 * Modifier)
+                {
+                    IgnoreNextHeadHit.Add(__instance.pilot);
+                }
+            }
         }
     }
-}
 
-[HarmonyPatch(typeof(BattleTech.Pilot), "SetNeedsInjury")]
-public static class PatchSetNeedsInjury
-{
-    /// <summary>
-    /// true implies injury occurs
-    /// </summary>
-    static bool Prefix(Pilot __instance, InjuryReason reason)
+    [HarmonyPatch(typeof(Pilot), "SetNeedsInjury")]
+    public static class Pilot_SetNeedsInjury_Patch
     {
-        var sim = UnityGameInstance.BattleTechGame.Simulation;
-        if (sim.CompanyTags.Contains("FewerHeadInjuriesDisabled"))
+        // true implies injury occurs
+        static bool Prefix(Pilot __instance, InjuryReason reason)
+        {
+            try
+            {
+                if (reason != InjuryReason.SideTorsoDestroyed &&
+                    reason == InjuryReason.HeadHit && IgnoreNextHeadHit.Contains(__instance))
+                {
+                    //var sim = UnityGameInstance.BattleTechGame.Simulation;
+                    //if (sim.CompanyTags.Contains("FewerHeadInjuriesDisabled"))
+                    //    return true;
+
+                    UnityGameInstance.BattleTechGame.Combat.MessageCenter.PublishMessage(
+                        new AddSequenceToStackMessage(
+                            new ShowActorInfoSequence(
+                                __instance.ParentActor,
+                                "INJURY AVOIDED",
+                                FloatieMessage.MessageNature.Inspiration,
+                                false)
+                        )
+                    );
+                    IgnoreNextHeadHit.Remove(__instance);
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString());
+            }
+
             return true;
-
-        if (reason == InjuryReason.HeadHit && FewerHeadInjuries.IgnoreNextHeadHit.Contains(__instance))
-        {
-            FewerHeadInjuries.IgnoreNextHeadHit.Remove(__instance);
-            return false;
         }
-
-        return true;
     }
 }
